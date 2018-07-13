704e1a81c46c5c6950e3a29880c69015c55da716
01Divergent <iphone.claudia@icloud.com>
2018-07-13 18:28:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # dog.rb
+class Dog 
+  attr_reader :breed, :name
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  # def name
+  #   name = @name
+  # end
+  # def name=(name)
+  #   @name = name
+  # end
+  # def age
+  #   age = @age
+  # end
+  # def age=(age)
+  #   @age = age
+  # end
+end
+vlad = Dog.new("Bowie", "Labrodoodle", "Almost 3", "cow spotted", "yes")
+sara = Dog.new("Cooper", "Aussie", "7", "Black", "yes")
+puts vlad.age = "3"
+puts vlad.name
+puts sara.trained
+puts sara.trained = "No"
