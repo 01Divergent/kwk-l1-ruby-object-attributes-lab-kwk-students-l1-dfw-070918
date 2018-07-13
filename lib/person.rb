e38a2db3e4f0a46ef9eec8d45371ab118bfd6aa9
01Divergent <iphone.claudia@icloud.com>
2018-07-13 19:16:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,16 @@
 # person.rb
 class Person
-  def name=(name)
-    @name = name
-  end
-  def name
+   def name
     @name
   end
-  def job=(job)
-    @job = job
+  def name=(name)
+    @name = name
   end
   def job
     @job
   end
+  def job=(job)
+    @job = job
+  end
+ 
 end
